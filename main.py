fc87da224fecde733d9ead3b04205c4f62f3de62
Erik Revaj <erik.revaj3@gmail.com>
2020-11-18 22:40:00 +0100

    created class Kettle

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.price)
 
-kenwood.price = 13
-print(kenwood.price)
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {0.make} {0.price} {1.make} {1.price}".format(kenwood, hamilton))
+print(hamilton.on)
+# hamilton.switch_on()
+print(hamilton.on)
+
+Kettle.switch_on(kenwood)
+print(kenwood.on)
